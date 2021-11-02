90d60dd604fe44d69caffe5721d1dc6d88dc1229
TomasRMendes <72995912+TomasRMendes@users.noreply.github.com>
2021-11-02 17:59:29 +0000

    guide

--- Assignment/TP1.py
+++ Assignment/TP1.py
@@ -1,4 +1,23 @@
-test
+"""
+standardize(train)
+standardize(test based on train)
+
+randomize(test)
+randomize(train)
+
+
+1.
+Naïve Bayes classifier using Kernel Density Estimation
+
+
+2.
+Gaussian Naïve Bayes classifier
+
+
+3.
+Support Vector Machine with a Gaussian radial basis function
+"""
+
 
 
 
