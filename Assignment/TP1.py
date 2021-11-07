ae8504e550895eeaac897d60438b73284b4d2ab9
Brockaaa <brocka.dev@gmail.com>
2021-11-07 13:58:39 +0100

    shuffeld and standartized the data

--- Assignment/TP1.py
+++ Assignment/TP1.py
@@ -1,6 +1,37 @@
+import numpy as np
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
+from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+
+
+mat_test = np.loadtxt('TP1_test.tsv',delimiter='\t')
+mat_train = np.loadtxt('TP1_train.tsv',delimiter='\t')
+
+
+# Ys contains the Classes (0 or 1)
+Ys_test = mat_test[:,-1]
+Ys_train = mat_train[:,-1]
+
+# Xs contains the coordinates (x1, x2)
+Xs_test = mat_test[:,:4]
+Xs_train = mat_train[:,:4]
+
+
+# standartization of coordinates
+means_train = np.mean(Xs_train,axis=0)
+stdevs_train = np.std(Xs_train,axis=0)
+# standardize(train)
+Xs_train = (Xs_train-means_train)/stdevs_train
+# standardize(test) based on train
+Xs_test = (Xs_test-means_train)/stdevs_train
+
+a
+
+
 """
-standardize(train)
-standardize(test based on train)
+
 
 randomize(test)
 randomize(train)
